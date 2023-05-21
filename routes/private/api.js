ed388886db908495a4267486032cfc621d81fa59
farahessammm <121769716+farahessammm@users.noreply.github.com>
2023-05-21 17:57:32 +0300

    senior request

--- routes/private/api.js
+++ routes/private/api.js
@@ -31,7 +31,16 @@ const getUser = async function (req) {
   user.isSenior = user.roleid === roles.senior;
   return user;
 };
-
+const getUserId = async function (req) {
+  const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    return res.status(301).redirect("/");
+  }
+  const userId = await db
+    .select("userId")
+    .from("se_project.sessions")
+    .where("token", sessionToken)
+}
 module.exports = function (app) {
   // example
   app.put("/users", async function (req, res) {
@@ -50,7 +59,18 @@ module.exports = function (app) {
   //starting habd el code 
   //reset password: 
 
-  app.put("/api/v1/password/reset",async function(req,res){
+  app.post("/api/v1/senior/request", async function (req, res) {
+    const nationalId = req.body.nationalId;
+    const userId = users.userId;
+    const seniorRequest = { nationalId: nationalId, userId: userId, status: "pending" }
+    try {
+      await db("se_project.senior_requests").insert(seniorRequest);
+      return res.status(200).send("Senior request is added successfully");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(400).send("Could not add nationalId");
+    }
+  });  app.put("/api/v1/password/reset",async function(req,res){
     try{
       const {newPassword } = req.body;
       const user = await getUser(req);
