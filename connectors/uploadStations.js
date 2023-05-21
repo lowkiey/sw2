81fd2c856316ceace13aa05f48a160e1cd7c080e
MartinaMaurice <126152704+MartinaMaurice@users.noreply.github.com>
2023-05-21 13:55:06 +0300

    upload s

--- connectors/uploadStations.js
+++ connectors/uploadStations.js
@@ -102,6 +102,6 @@ async function uploadR() {
     await db("se_project.routes").insert(element).returning("*");
   }
 }
- uploadS(); //first to run
+// uploadS(); //first to run
 // uploadR(); //second
-//uploadSR(); //third
+// uploadSR(); //third
